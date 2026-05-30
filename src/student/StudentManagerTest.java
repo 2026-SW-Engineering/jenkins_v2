170c031000d23c3c8458d23a9458a8334c2168fe
soyeoneeii <syp0463@gmail.com>
2026-05-30 16:36:49 +0900

    feat: student class and Junit test file

---
+++ src/student/StudentManagerTest.java
@@ -0,0 +1,55 @@
+package student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StudentManagerTest {
+	
+    private static StudentManager manager;
+    
+    @BeforeAll
+    static void beforeAll() {
+        manager = new StudentManager();
+        manager.addStudent("Alice");
+    }
+    
+
+	@Test
+	@Order(1)
+	void testAddStudent() {
+		assertTrue(manager.hasStudent("Alice"));
+	}
+
+	@Test
+	@Order(2)
+	void testRemoveStudent() {
+		manager.removeStudent("Alice");
+		assertFalse(manager.hasStudent("Alice"));
+	}
+
+    @Test
+    @Order(3)
+    void testDuplicateAddStudentThrowsException() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            manager.addStudent("Alice");
+        });
+    }
+    
+    @Test
+    @Order(4)
+    void testRemoveNonExistingStudentThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            manager.removeStudent("Bob");
+        });
+    }
+    
+
+}
